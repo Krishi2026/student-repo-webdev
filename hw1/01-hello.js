6dfb8eeacbb07458bf3122c8646b4cfadb1d814a
Krishna Karthik Chava <146298567+Krishi2026@users.noreply.github.com>
2024-01-30 06:32:17 +0530

    [ISSUEFIX]:Have added changes related to my first assignment for Hw1 folder in html css and JavaScript files and also did all checklist changes  branch main

--- hw1/01-hello.js
+++ hw1/01-hello.js
@@ -1,5 +1,8 @@
-window.addEventListener('load', loadPage);
+window.addEventListener("load", loadPage);
 
 function loadPage(event) {
-  console.log('The page has now loaded.');
+  console.log("The page has now loaded.");
+  console.log(
+    "My name is Krishna Karthik Chava and I am a dedicated graduate student currently pursuing a Master's degree in Computer Science at Portland State University, driven by a passion for exploring the realms of Computer Science."
+  );
 }
